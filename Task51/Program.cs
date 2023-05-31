8a39cf97f7f0c928e2266471aac1fddffbb1f9ed
GachinAndrey <gachin.andrei@gmail.com>
2023-05-31 21:53:02 +0300

    add Task46, 48, 49, 51

---
+++ Task51/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 51: Задайте двумерный массив. Найдите сумму
+элементов, находящихся на главной диагонали (с индексами
+(0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+int[,] CreateMatrixFormula(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows = arr.GetLength (0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // columns = arr.GetLength (1)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} "); // 5 - форматирование отступа
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumElementsOnDiagonal(int[,] matr)
+{
+    int sum = 0;
+    //int size = matr.GetLength(0) > matr.GetLength(1) ? matr.GetLength(1) : matr.GetLength(0);
+    //for (int i = 0; i < size; i++)
+    for (int i = 0; i < matr.GetLength(0) && i < matr.GetLength(1); i++)
+    {
+        sum += matr[i, i];
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите число строк");
+int rows2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int columns2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное число в массиве");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное число в массиве");
+int maximum = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2d = CreateMatrixFormula(rows2, columns2, minimum, maximum); //лучше пользовательский ввод
+PrintMatrix(array2d);
+int sumElementsOnDiagonal = SumElementsOnDiagonal(array2d);
+Console.WriteLine($"Сумма элементов главной диагонали равна {sumElementsOnDiagonal}");
